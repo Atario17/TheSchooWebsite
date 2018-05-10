495f2d94ea7efb5864afebfec5cf65d50a5a9740
Atario17 <daryatikhomirova@protonmail.com>
2018-05-10 19:56:07 +0300

    Создали проект

---
+++ src/main/java/com/kondrat/TheSchooWebsite/TheSchooWebsiteApplication.java
@@ -0,0 +1,12 @@
+package com.kondrat.TheSchooWebsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheSchooWebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheSchooWebsiteApplication.class, args);
+	}
+}
---
+++ src/test/java/com/kondrat/TheSchooWebsite/TheSchooWebsiteApplicationTests.java
@@ -0,0 +1,16 @@
+package com.kondrat.TheSchooWebsite;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TheSchooWebsiteApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
